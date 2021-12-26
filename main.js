d734f553cdf69999297de78486a14af47738114f
shivangijajodia <96367839+shivangijajodia@users.noreply.github.com>
2021-12-26 16:20:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+song="";
+
+function preload(){
+song=loadSound("music.mp3");
+}
+function setup(){
+    canvas=createCanvas(650,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,650,500);
+}
+function play(){
+    song.play();
+}
